36292f4565ce6d8556b63bf3a1e5ede2a972ccf4
Renato Oliveira <rc.oliveira@samsung.com>
2021-03-01 08:48:07 -0300

    Adding new activity for result displaying

--- JavaKeywords/app/src/main/java/com/example/javakeywords/MainActivity.kt
+++ JavaKeywords/app/src/main/java/com/example/javakeywords/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.javakeywords
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.KeyEvent
@@ -10,9 +11,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.javakeywords.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    private val wordsList = listOf("abstract",
+    private val wordsList = listOf(
+        "abstract",
         "continue",
         "for",
         "new",
@@ -61,7 +64,8 @@ class MainActivity : AppCompatActivity() {
         "float",
         "native",
         "super",
-        "while")
+        "while"
+    )
 
     private lateinit var remainingWords: MutableList<String>
 
@@ -69,18 +73,30 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val timer = object :CountDownTimer(180000,
-        1000) {
+    private val timer = object : CountDownTimer(
+        180000,
+        1000
+    ) {
 
         override fun onTick(millisUntilFinished: Long) {
             val date = Date(millisUntilFinished)
             val format = SimpleDateFormat("mm:ss")
-            binding.timer.text = getString(R.string.time_left,
-                format.format(date))
+            binding.timer.text = getString(
+                R.string.time_left,
+                format.format(date)
+            )
         }
 
         override fun onFinish() {
             binding.guessWord.isEnabled = false
+
+            val context = binding.root.context
+
+            val intent = Intent(context, ResultActivity::class.java)
+            intent.putExtra("score", score)
+            intent.putStringArrayListExtra("remainingWords", ArrayList(remainingWords))
+            context.startActivity(intent)
+            /*
             binding.remainingWords.append("\n")
             binding.remainingWords.append("WORDS NOT GUESSED:")
 
@@ -88,7 +104,7 @@ class MainActivity : AppCompatActivity() {
                     .forEach {
                         binding.remainingWords.append("\n")
                         binding.remainingWords.append(it)
-                    }
+                    }*/
         }
     }
 
@@ -98,13 +114,19 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.score.text = getString(R.string.score,
-            0.toString())
-        binding.timer.text = getString(R.string.time_left,
-            "3:00")
+        binding.score.text = getString(
+            R.string.score,
+            0.toString()
+        )
+        binding.timer.text = getString(
+            R.string.time_left,
+            "3:00"
+        )
         binding.guessWord.setOnKeyListener { view, keyCode, _ ->
-            handleKeyEvent(view,
-                keyCode)
+            handleKeyEvent(
+                view,
+                keyCode
+            )
         }
         binding.guessWord.isEnabled = false
 
@@ -120,32 +142,47 @@ class MainActivity : AppCompatActivity() {
         binding.wordsList.text = "WORDS GUESSED SO FAR:"
         binding.remainingWords.text = ""
         score = 0
-        binding.score.text = getString(R.string.score,
-            score.toString())
-        binding.timer.text = getString(R.string.time_left,
-            "3:00")
+        binding.score.text = getString(
+            R.string.score,
+            score.toString()
+        )
+        binding.timer.text = getString(
+            R.string.time_left,
+            "3:00"
+        )
         timer.start()
     }
 
-    private fun handleKeyEvent(view: View,
-                               keyCode: Int): Boolean {
+    private fun handleKeyEvent(
+        view: View,
+        keyCode: Int
+    ): Boolean {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
             val guessWord: String = binding.guessWord.text.toString()
-                    .toLowerCase()
+                .toLowerCase()
             if (remainingWords.contains(guessWord)) {
                 binding.wordsList.append("\n")
                 binding.wordsList.append(guessWord)
                 score++
-                binding.score.text = getString(R.string.score,
-                    score.toString())
+                binding.score.text = getString(
+                    R.string.score,
+                    score.toString()
+                )
                 remainingWords.remove(guessWord)
-                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                inputMethodManager.hideSoftInputFromWindow(view.windowToken,
-                    0)
+                val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(
+                    view.windowToken,
+                    0
+                )
                 return true
             }
             binding.guessWord.text.clear()
         }
         return false
+    }
+
+    fun getListOfWords(): MutableList<String> {
+        return remainingWords
     }
 }
