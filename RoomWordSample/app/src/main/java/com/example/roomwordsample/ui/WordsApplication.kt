131f7dc37f79fd4beaa6f58800ddcab7d39d235d
Renato Cesar Alves de Oliveira <RenatoCesar.Oliveira-ext@ab-Inbev.com>
2022-06-24 13:27:34 -0300

    [ROOM_SAMPLE] Adding room sample project

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/ui/WordsApplication.kt
@@ -0,0 +1,15 @@
+package com.example.roomwordsample.ui
+
+import android.app.Application
+import com.example.roomwordsample.data.WordRepository
+import com.example.roomwordsample.model.WordRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class WordsApplication : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
